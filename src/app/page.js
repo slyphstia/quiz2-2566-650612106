d3d8eb88748b06ef8dfccdb5578643ce26d3cc98
hghgh <apiwit_boonyarit@cmu.ac.th>
2023-08-10 15:34:32 +0700

    almost finish

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 "use client";
 
+import { Comment } from "@/components/Comment";
+import { PostOwner } from "@/components/PostOwner";
+import { Reply } from "@/components/Reply";
+import { comments } from "@/libs/comments";
+
 export default function HomePage() {
   return (
     <div
@@ -12,73 +17,46 @@ export default function HomePage() {
         className="mx-auto p-3 rounded rounded-3 shadow-sm bg-white"
       >
         {/* Post Owner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/handsome.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5">Chayanin Suatap 650610560</span>
-          </div>
+        <PostOwner name="Apiwit Boonyarit" id="650612106" comment="Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207"></PostOwner>
 
-          <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
+      
+        
+       {comments.map((x,i)=> (
+        <Comment
+          key={i+1}
+          userImagePath={x.userImagePath}
+          username={x.username}
+          commentText={x.commentText}
+          likeNum={x.likeNum}
+          replies={[x.replies]}
+        />
 
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span className="text-muted">100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div>
 
-        {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#E5E7EB" }}
-          >
-            <span className="fw-semibold">Lisa</span>
-            <br />
-            <span>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span className="text-muted">999 คน</span>
-            </div>
-          </div>
-        </div>
+       ))}
 
-        {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#E5E7EB" }}
-          >
-            <span className="fw-semibold">หมาน้อย</span>
-            <br />
-            <span>จริงค้าบบบบบบบบ</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span className="text-muted">2 คน</span>
-            </div>
-          </div>
-        </div>
+       {comments.map((x,i)=> (
+        <Reply
+          key={i+1}
+          userImagePath={[x.replies.userImagePath]}
+          username={[x.replies.username]}
+          replyText={[x.replies.replyText]}
+          likeNum={[x.replies.likeNum]}
+        
+        
+        
+        />
+       ))}
 
+       
+       
+       
+  
+        
+        
+        
+        
+
+        
         {/* map-loop render Comment component here */}
       </div>
     </div>
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,10 +5,35 @@ export const Comment = ({
   likeNum,
   replies,
 }) => {
+  let check = 0 ;
+   replies = [];
   return (
-    <div>
-      your code here...
+    <div >
+     
       {/* map-loop render Reply component here */}
+      <div className="d-flex gap-2 my-2">
+          <img
+            src={userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#E5E7EB" }}
+          >
+            <span className="fw-semibold">{username}</span>
+            <br />
+            <span>{commentText}</span>
+            <div className="d-flex align-items-center gap-1">
+              {likeNum > check ? <img src="/like.svg" width={20}></img> :  ""}
+              {likeNum > check ? <span className="text-muted">{likeNum}</span> :""}
+            </div>
+          </div>
+        </div>
+
+
     </div>
   );
 };
